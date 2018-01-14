93d4f26a181041d85d0771137b80ddb915e2e09a
Suraj Pinto <SurajPinto000@gmail.com>
2018-01-14 04:03:21 -0800

    Assignment 1   Part 2
    divisible by 3 and 5   FizzFuzz
    divisible by 5   Fizz
    divisible by 7   Fuzz
    default   display just number

--- Assignment01/js/Assignment01.js
+++ Assignment01/js/Assignment01.js
@@ -1,7 +1,10 @@
 
 for (var i = 1; i <= 300; i++) {
     
-    if (i % 5 === 0) {
+    if ((i % 3 === 0) && (i % 5 === 0)) {
+        console.log("FizzFuzz");
+    }
+    else if (i % 5 === 0) {
         console.log("Fizz");
     }
     else if (i % 7 === 0) {

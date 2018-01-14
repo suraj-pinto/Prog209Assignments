53df25776ec3930c7dc94986b17617a413873606
Suraj Pinto <SurajPinto000@gmail.com>
2018-01-14 03:13:57 -0800

    Initial commit

---
+++ Assignment01/js/Assignment01.js
@@ -0,0 +1,13 @@
+
+for (var i = 1; i <= 300; i++) {
+    
+    if (i % 5 === 0) {
+        console.log("Fizz");
+    }
+    else if (i % 7 === 0) {
+        console.log("Fuzz");
+    }
+    else {
+        console.log(i);
+    }
+}
---
+++ README.md
@@ -0,0 +1,1 @@
+# Prog209Assignments
